199ad65d106b0dd4cb964580246496ff562704fb
candra <candraherdiansyah14@gmail.com>
2022-07-25 15:04:19 +0700

    Seeder Users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = "admin";
+        $user->email = "admin@gmail.com";
+        $user->password = bcrypt("rahasia");
+        $user->save();
+
+    }
+}
